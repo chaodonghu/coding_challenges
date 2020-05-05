ce23ec36dd84330b7259d118252a7fab1845c36c
chaodonghu <cdhu@uwaterloo.ca>
2020-05-05 11:04:24 -0400

    add js solution to first unique character in a string

---
+++ leetcode/387_first_unique_character_in_a_string.js
@@ -0,0 +1,40 @@
+// Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+//
+// Examples:
+//
+// s = "leetcode"
+// return 0.
+//
+// s = "loveleetcode",
+// return 2.
+// Note: You may assume the string contain only lowercase letters.
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    // hashmap at most will contain all letters of the alphabet (26 key-value pairs)
+    let characterCount = {};
+
+    // loop through string once
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        characterCount[char] = characterCount[char] + 1 || 1;
+    }
+
+    // loop through string again to return back index
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (characterCount[char] === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+// Time Complexity: O(N) we loop through the input string twice
+// (1) to populate hash map
+// (2) to find the index of the first character that has only occured once
+
+// Space Complexity: O(1) hashmap will atmost have all letters of the alphabet (26 key-value pairs)
