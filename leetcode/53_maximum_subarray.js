bbfd5965cf66ab7b639251c54023a4212f39e0d6
chaodonghu <cdhu@uwaterloo.ca>
2020-05-04 22:40:50 -0400

    add js solution for maximum subarray

---
+++ leetcode/53_maximum_subarray.js
@@ -0,0 +1,29 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+//
+// Example:
+//
+// Input: [-2,1,-3,4,-1,2,1,-5,4],
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Follow up:
+//
+// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let currentMax = nums[0];
+  let globalMax = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentMax = Math.max(nums[i], currentMax + nums[i]);
+
+    if (currentMax > globalMax) {
+      globalMax = currentMax;
+    }
+  }
+
+  return globalMax;
+};
