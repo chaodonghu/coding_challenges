0072f1cf342cc63b5c472d494027aa1b75e98760
chaodonghu <cdhu@uwaterloo.ca>
2020-05-09 14:34:17 -0400

    add js solution to move zeros

---
+++ leetcode/283_move_zeros.js
@@ -0,0 +1,30 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// Example:
+//
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Note:
+//
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    // anchor refers to the index in nums
+    let anchor = 0;
+
+    for (let explorer = 0; explorer < nums.length; explorer++) {
+        if (nums[explorer] !== 0) {
+            // temp will always be 0;
+            let temp = nums[anchor];
+            nums[anchor] = nums[explorer];
+            nums[explorer] = temp;
+
+            anchor++;
+        }
+    }
+
+};
