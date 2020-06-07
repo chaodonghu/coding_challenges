90850ea0e6305af9ba106e533663cb72ad1a06d8
chaodonghu <cdhu@uwaterloo.ca>
2020-06-07 11:10:05 -0400

    add dp solution to coin change 2

---
+++ leetcode/518_coin_change_2.js
@@ -0,0 +1,54 @@
+// You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+//
+//
+//
+// Example 1:
+//
+// Input: amount = 5, coins = [1, 2, 5]
+// Output: 4
+// Explanation: there are four ways to make up the amount:
+// 5=5
+// 5=2+2+1
+// 5=2+1+1+1
+// 5=1+1+1+1+1
+// Example 2:
+//
+// Input: amount = 3, coins = [2]
+// Output: 0
+// Explanation: the amount of 3 cannot be made up just with coins of 2.
+// Example 3:
+//
+// Input: amount = 10, coins = [10]
+// Output: 1
+//
+//
+// Note:
+//
+// You can assume that
+//
+// 0 <= amount <= 5000
+// 1 <= coin <= 5000
+// the number of coins is less than 500
+// the answer is guaranteed to fit into signed 32-bit integer
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+  const coinLength = coins.length
+
+  const DP = Array(amount + 1).fill(0)
+
+  DP[0] = 1
+
+  for (const coin of coins) {
+    for (let col = coin; col <= amount; col++) {
+      // populate the column/index with the previous value (subproblem)
+      DP[col] += DP[col - coin]
+    }
+  }
+    // take the last index of the dp
+    return DP[amount]
+};
