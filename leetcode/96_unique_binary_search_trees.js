f1395368578c1e4ac4da7d311585b42eea0a4c24
chaodonghu <cdhu@uwaterloo.ca>
2020-06-24 19:28:06 -0400

    add dp solution to unique binary search trees

---
+++ leetcode/96_unique_binary_search_trees.js
@@ -0,0 +1,30 @@
+// Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+//
+// Example:
+//
+// Input: 3
+// Output: 5
+// Explanation:
+// Given n = 3, there are a total of 5 unique BST's:
+//
+//    1         3     3      2      1
+//     \       /     /      / \      \
+//      3     2     1      1   3      2
+//     /     /       \                 \
+//    2     1         2                 3
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+  let G = new Array(n + 1).fill(0);
+  G[0] = 1;
+  G[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      G[i] += G[j - 1] * G[i - j];
+    }
+  }
+  return G[n];
+};
