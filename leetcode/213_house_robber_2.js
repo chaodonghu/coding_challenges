8ac65b516c5952d50f9376003d7e5afb4fb4c97d
chaodonghu <cdhu@uwaterloo.ca>
2020-07-09 01:05:59 -0400

    modify house robber 2 to utilize loot logic from house robber 1

--- leetcode/213_house_robber_2.js
+++ leetcode/213_house_robber_2.js
@@ -2,34 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
-var loot = function (nums) {
-  let loot = [];
-
-  if (!nums.length) return 0;
-
-  nums.forEach((num, i) => {
-    if (i === 0) {
-      loot.push(num);
-    } else if (i === 1) {
-      loot.push(Math.max(num, loot[i - 1]));
-    } else {
-      loot.push(Math.max(loot[i - 1], num + loot[i - 2]));
-    }
-  });
-
-  return loot[loot.length - 1];
-};
+ var loot = function(nums) {
+   // Instead of creating an array to save all of the previous maxes just take the previous two maxes
+   // We only need to take the previous two maxes
+   // because at most we would only go two spaces away
+   // to check our maximum value
+   let prevMax = 0;
+   let twoAwayFromMax = 0;
+   nums.forEach(house => {
+     // destructuring assignment to swap prevMax and twoAwayFromMax values
+     [prevMax, twoAwayFromMax] = [
+       Math.max(house + twoAwayFromMax, prevMax),
+       prevMax
+     ];
+   });
+   return prevMax;
+ };
 
 var rob = function (nums) {
   let length = nums.length;
 
   if (length === 0) {
-    return 1;
+    return 0;
   }
 
   if (length <= 3) {
-    return Math.max(nums);
+    return Math.max.apply(null, nums);
   }
 
   return Math.max(loot(nums.slice(0, -1), loot(nums.slice(1))));
+    // return loot(nums.slice(0, -1));
 };
