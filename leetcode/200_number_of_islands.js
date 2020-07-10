a90cc13cac39b4a3c1d4b3069f40c52cc4cf4d2e
chaodonghu <cdhu@uwaterloo.ca>
2020-07-10 00:48:08 -0400

    add dfs solution to number of islands

---
+++ leetcode/200_number_of_islands.js
@@ -0,0 +1,54 @@
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+//
+// Example 1:
+//
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+// Example 2:
+//
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+var numIslands = function (grid) {
+  let count = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "1") {
+        count++;
+        dfs(grid, row, col);
+      }
+    }
+  }
+
+  return count;
+};
+
+var dfs = function (grid, row, col) {
+  if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
+
+  const value = grid[row][col];
+
+  if (value === "1") {
+    grid[row][col] = "#";
+    dfs(grid, row - 1, col);
+    dfs(grid, row + 1, col);
+    dfs(grid, row, col - 1);
+    dfs(grid, row, col + 1);
+  }
+};
