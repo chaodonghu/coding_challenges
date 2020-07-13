f8b3537ef377865eecc6c9d7f92810f5a97b1cfb
chaodonghu <cdhu@uwaterloo.ca>
2020-07-13 14:57:09 -0400

    add tortoise and hare solution to list linked cycle

---
+++ leetcode/141_linked_list_cycle.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fastPointer = head;
+    let slowPointer = head;
+};
