9975b24f36e2fd2f8c326c888629d524b296e4a1
chaodonghu <dojostan@gmail.com>
2020-07-22 19:29:55 -0400

    add dfs solution to binary tree zigzag level order traversal

---
+++ leetcode/103_binary_tree_zigzag_level_order_traversal.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  // instantiate our output stack
+  let output = [];
+
+  // do a dfs
+  var dfs = function (node, level, output) {
+    if (!node) return;
+
+    // if the output level array doesn't exist yet create it
+    if (!output[level]) {
+      output.push([]);
+    }
+
+    // go to the left and the right of the root and increment the level
+    dfs(node.left, level + 1, output);
+    dfs(node.right, level + 1, output);
+    if (level % 2 === 0) {
+      output[level].push(node.val);
+    } else {
+      output[level].unshift(node.val);
+    }
+  };
+
+  dfs(root, 0, output);
+
+  return output;
+};
