f3383bc528cef3612be961ebac7b139244fc0f00
chaodonghu <dojostan@gmail.com>
2020-07-26 00:25:47 -0400

    add cleaner binary search solution to find minimum in rotated sorted array

--- leetcode/154_find_minimum_in_rotated_sorted_array_ii.js
+++ leetcode/154_find_minimum_in_rotated_sorted_array_ii.js
@@ -1,47 +1,42 @@
-// gotcha: [2,1,2,2,2]
-// the repeats could wrap around the whole array, such that, left, mid, right are all the same
-// so because arr[left] === arr[mid] === arr[right], the only thing we can do is shave off
-// one element one at a time, not knowing when a non-duplicate will be seen
-
-// so [2,1,2,2,2] becomes [1,2,2,2]
-// arr[left] no longer equals arr[mid], but then arr[mid] === arr[right]
-
-// so we shave off elements from the right..
-// [1,2,2,2] => ...zzz.... becomes => [1,2]
-
-// and we're done!
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+//
+// (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+//
+// Find the minimum element.
+//
+// The array may contain duplicates.
+//
+// Example 1:
+//
+// Input: [1,3,5]
+// Output: 1
+// Example 2:
+//
+// Input: [2,2,2,0,1]
+// Output: 0
+// Note:
+//
+// This is a follow up problem to Find Minimum in Rotated Sorted Array.
+// Would allow duplicates affect the run-time complexity? How and why?
 
 var findMin = function (nums) {
-  if (!nums || !nums.length) return -1;
-  return binarySearch(nums, { left: 0, right: nums.length - 1 });
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+
+    if (nums[mid] < nums[right]) {
+      right = mid;
+    } else if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right --;
+    }
+  }
+
+  return nums[left];
 };
 
-function binarySearch(arr, { left, right }) {
-  if (left >= right) return arr[left]; // we have ended at one element
-
-  let mid = Math.floor((left + right) / 2);
-
-  // if left element = mid element, it means everyhting in between is also useless... right?
-  if (left !== mid && arr[left] === arr[mid]) {
-    // but because there could be a min between left and mid, we can't just drop everything between left<->mid...
-    // return binarySearch(arr, { left:mid, right}) <-- SO THIS IS WRONG!!!
-
-    // instead we only drop 1 element at a time
-    return binarySearch(arr, { left: left + 1, right });
-  }
-
-  // same logic as above
-  if (right !== mid && arr[mid] === arr[right]) {
-    return binarySearch(arr, { left, right: right - 1 });
-  }
-
-  // check whether right side is increasing, if so, we know its useless, and should only look to the left
-  const isMidToRightAscending = arr[right] > arr[mid];
-
-  if (isMidToRightAscending) {
-    // but note that for [3,1,2], mid could still contain element, so dont use right=mid-1
-    return binarySearch(arr, { left, right: mid });
-  }
-
-  return binarySearch(arr, { left: mid + 1, right });
-}
+// duplicates would affect the run-time complexity as if we had an array of all the same number
+// eg. [3, 3, 3, 3, 3, 3], at worse it would be O(N) since we would have to go through all the elements
+// rather than if we had a definite pivot we could do it in O(log N)
