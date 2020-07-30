7334496615345ffe09c55beee6bd82132212fbcb
chaodonghu <dojostan@gmail.com>
2020-07-29 21:22:24 -0400

    correct solution to minimum path sum utilizing dp array

--- leetcode/64_minimum_path_sum.js
+++ leetcode/64_minimum_path_sum.js
@@ -1,14 +1,43 @@
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+//
+// Note: You can only move either down or right at any point in time.
+//
+// Example:
+//
+// Input:
+// [
+//   [1,3,1],
+//   [1,5,1],
+//   [4,2,1]
+// ]
+// Output: 7
+// Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
 /**
  * @param {number[][]} grid
  * @return {number}
  */
 var minPathSum = function (grid) {
-  let sum = grid[0][0];
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 1; j < grid[0].length; j++) {
-      sum += Math.min(grid[i][j], grid[i + 1][j]);
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // modify the columns
+  for (let i = 1; i < m; i++) {
+    grid[i][0] += grid[i - 1][0];
+  }
+
+  // modify the rows
+  for (let i = 1; i < n; i++) {
+    grid[0][i] += grid[0][i - 1];
+  }
+
+  // go through entire modified grid and add to the current implementation starting at coordinate
+  // (1, 1), either the minimum of the previous upper cell or previous left cell
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
     }
   }
 
-  console.log('sum', sum);
+  return grid[m - 1][n - 1];
 };
