d8ef36bd98738e7480568fa44bfcf95b1a1bb6b5
chaodonghu <dojostan@gmail.com>
2020-11-30 00:23:52 -0500

    add recursive solution to jump game iii

---
+++ leetcode/1306_jump_game_iii.js
@@ -0,0 +1,60 @@
+// Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+//
+// Notice that you can not jump outside of the array at any time.
+//
+//
+//
+// Example 1:
+//
+// Input: arr = [4,2,3,0,3,1,2], start = 5
+// Output: true
+// Explanation:
+// All possible ways to reach at index 3 with value 0 are:
+// index 5 -> index 4 -> index 1 -> index 3
+// index 5 -> index 6 -> index 4 -> index 1 -> index 3
+// Example 2:
+//
+// Input: arr = [4,2,3,0,3,1,2], start = 0
+// Output: true
+// Explanation:
+// One possible way to reach at index 3 with value 0 is:
+// index 0 -> index 4 -> index 1 -> index 3
+// Example 3:
+//
+// Input: arr = [3,0,2,1,2], start = 2
+// Output: false
+// Explanation: There is no way to reach at index 1 with value 0.
+//
+//
+// Constraints:
+//
+// 1 <= arr.length <= 5 * 10^4
+// 0 <= arr[i] < arr.length
+// 0 <= start < arr.length
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+var canReach = function(arr, start) {
+    var recur = (cur) => {
+        // if current index is out of bounds or the current index value is negative
+        if (cur < 0 || cur >= arr.length || arr[cur] < 0) {
+            return false;
+        }
+
+        if (arr[cur] === 0) {
+            return true;
+        }
+
+        // set the current index as visited
+        arr[cur] = -arr[cur];
+
+        // recursively call the current index plus its value or current index minus it's value until we are either out of bounds or we reach an index that equals 0
+        return (recur(arr[cur] + cur) || recur(cur - arr[cur]));
+    }
+
+    return recur(start);
+};
