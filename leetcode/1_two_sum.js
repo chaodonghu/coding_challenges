3e1d1696114254cd0d5f19ad1fc1b6b788339639
chaodonghu <dojostan@gmail.com>
2020-12-02 16:32:05 -0500

    add brute force solution to two sum

--- leetcode/1_two_sum.js
+++ leetcode/1_two_sum.js
@@ -3,6 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// Brute force -> O(N^2)
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// O(N) hash map
 var twoSum = function (nums, target) {
   let map = new Map();
 
