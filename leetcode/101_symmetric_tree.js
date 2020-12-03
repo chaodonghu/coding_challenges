b8ac6d83b8c2d1f6b8f4ec84779700319c26a60b
chaodonghu <cdhu@uwaterloo.ca>
2020-12-02 22:13:56 -0500

    add recursive solution to symmetric tree

---
+++ leetcode/101_symmetric_tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  let isMirror = (leftNode, rightNode) => {
+    if (!leftNode && !rightNode) return true;
+    if (!leftNode || !rightNode) return false;
+
+    return (
+      leftNode.val === rightNode.val &&
+      isMirror(leftNode.left, rightNode.right) &&
+      isMirror(leftNode.right && rightNode.left)
+    );
+  };
+
+  isMirror(root, root);
+};
