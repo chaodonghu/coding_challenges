1f39d5a8f48c3a246124cd814aaeab7403e662b7
chaodonghu <dojostan@gmail.com>
2021-02-02 18:43:18 -0500

    add sorted union solution

---
+++ algorithms/sorted_union.js
@@ -0,0 +1,25 @@
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+//
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+//
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+//
+// Check the assertion tests for examples.
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+//
+// Passed
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+//
+// Passed
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+
+function uniteUnique(...arr) {
+  let flattedArray = arr.reduce((acc, el) => {
+    return acc.concat(el);
+  });
+
+  return [...new Set(flattedArray)];
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
