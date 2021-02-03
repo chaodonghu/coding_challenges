ab989fa901730f0ad361d22b8ae8df4317b40861
chaodonghu <dojostan@gmail.com>
2021-02-03 12:31:32 -0500

    add tortoise and hare solution to linked list cycle

--- leetcode/141_linked_list_cycle.js
+++ leetcode/141_linked_list_cycle.js
@@ -11,6 +11,25 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let fastPointer = head;
-    let slowPointer = head;
+  if (!head) return false;
+
+  let tortoise = head;
+  let hare = head;
+
+  while (hare && hare.next) {
+    // hare travels at two increments
+    hare = hare.next.next;
+    // tortoise travels at one increment
+    tortoise = tortoise.next;
+
+    // at one point they will intersect
+    if(tortoise === hare) {
+      return true;
+    }
+  }
+
+  return false;
 };
+
+// The hare will run at a speed of ax where a is a constant and the tortoise will run at a speed of bx where b is a constant and a > b,
+// at one point the will intersect
