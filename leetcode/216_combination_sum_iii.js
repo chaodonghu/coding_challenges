cba03f6c496e07c91ad215c6d5836791793345d8
chaodonghu <dojostan@gmail.com>
2021-02-12 13:32:52 -0500

    add solution to combination sum iii

--- leetcode/216_combination_sum_iii.js
+++ leetcode/216_combination_sum_iii.js
@@ -12,3 +12,53 @@
 //
 // Input: k = 3, n = 9
 // Output: [[1,2,6], [1,3,5], [2,3,4]]
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+  let result = [];
+  let permute = (arr, sum, index) => {
+    if (sum > n) return;
+    if (arr.length === k) {
+      if (sum === n) return result.push(arr);
+    }
+
+    for (let i = index; i < 10; i++) {
+      let newArray = [...arr, i];
+      let newSum = sum + i;
+      permute(newArray, newSum, i + 1);
+    }
+  };
+
+  permute([], 0, 1);
+
+  return result;
+};
+
+// []
+// [1]
+// [1, 2]
+// [1, 2, 3]
+// [1, 2, 3, 4]
+// [1, 2, 3, 5]
+// [1, 2, 3, 6]
+// [1, 2, 3, 7]
+// [1, 2, 3, 8]
+// [1, 2, 3, 9]
+// [1, 2, 4]
+// [1, 2, 5]
+// [1, 2, 6]
+// [1, 2, 7]
+// [1, 2, 8]
+// [1, 2, 9]
+// [1, 3]
+// [1, 3, 4]
+// [1, 3, 5]
+// [1, 3, 6]
+// [1, 3, 7]
+// [1, 3, 8]
+// [1, 3, 9]
+// [1, 4]
+// [1, 4, 5]
