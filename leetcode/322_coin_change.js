b0d063aff913e7c47e31c2feb816bcd784fdacc5
chaodonghu <dojostan@gmail.com>
2021-02-17 19:08:05 -0500

    add space and time complexity explanation to coin change

--- leetcode/322_coin_change.js
+++ leetcode/322_coin_change.js
@@ -17,22 +17,26 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    // instantiate a dp array a length of the amount and fill it with the maximum possible number
-    const dp = new Array(amount + 1).fill(Number.MAX_VALUE);
-    // first element in the dp array is 0
-    dp[0] = 0;
+var coinChange = function (coins, amount) {
+  // instantiate a dp array a length of the amount and fill it with the maximum possible number
+  const dp = new Array(amount + 1).fill(Number.MAX_VALUE);
+  // first element in the dp array is 0
+  dp[0] = 0;
 
-    // loop through the dp array
-    for (let i = 1; i <= amount; i++) {
-        // loop through the coins
-        for (let j = 0; j < coins.length; j++) {
-            // if the current amount subtracted by the current coin is greater than 0
-            if (i - coins[j] >= 0) {
-                // take the minimum of the dp of current element - the coin element and add one, comparing with the existing Number.MAX_VALUE
-                dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
-            }
-        }
+  // loop through the dp array
+  // build the solution bottom up by solving subproblems, coins that equate all the way up to the amount
+  for (let a = 1; a < amount + 1; a++) {
+    for (let coin of coins) {
+      // if our amount subtracted by the coin we are on right now has a remainder or is 0
+      if (a - coin >= 0) {
+        // add our current coin which is 1 and the subproblems
+        dp[a] = Math.min(dp[a], 1 + dp[a - coin]);
+      }
     }
-    return dp[amount] === Number.MAX_VALUE ? -1 : dp[amount];
+  }
+  // if our amount is not the default value then we have coins that can add up to it, if not then return -1
+  return dp[amount] === Number.MAX_VALUE ? -1 : dp[amount];
 };
+
+// Time Complexity: O(len(coins) * amount)
+// Space Complexity: O(amount) since we create a dynamic array with all the values of each coins that add up to that amount/index
