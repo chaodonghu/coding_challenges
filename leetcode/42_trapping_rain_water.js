b8547ec11fefdefb768e17e3253bb4ff669decd8
chaodonghu <dojostan@gmail.com>
2021-02-21 23:36:56 -0500

    add dynamic programming solution to trapping rain water

--- leetcode/42_trapping_rain_water.js
+++ leetcode/42_trapping_rain_water.js
@@ -1,41 +1,38 @@
-// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
-//
-//
-// The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
-//
-// Example:
-//
-// Input: [0,1,0,2,1,0,1,3,2,1,2,1]
-// Output: 6
-
 /**
  * @param {number[]} height
  * @return {number}
  */
-let trap = function (height) {
-  if (height === null) {
-    return 0;
-  }
+var trap = function(height) {
+    if (!height) return 0;
 
-  let totalWater = 0;
-  let length = height.length;
-  let leftMax = [];
-  let rightMax = [];
-  leftMax[0] = height[0];
+    let leftMax = [];
+    let rightMax = [];
+    let totalWater = 0;
+    let length = height.length;
+    // left max instantiate it as the first height in our array
+    leftMax[0] = height[0];
+    // right max instantiate it as the last height in our array
+    rightMax[length-1] = height[length - 1];
 
-  for (let i = 1; i < length; i++) {
-    leftMax[i] = Math.max(height[i], leftMax[i - 1]);
-  }
 
-  rightMax[length - 1] = height[length - 1];
+    for (let i = 1; i < length; i++) {
+        // the left max at the index is either the max of the height currently or our previous max
+        leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    }
 
-  for (let i = length - 2; i >= 0; i--) {
-    rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-  }
+    // go from right to left, start at second last index
+    for (let j = length - 2; j >= 0; j--) {
+        rightMax[j] = Math.max(height[j], rightMax[j + 1]);
+    }
 
-  for (let i = 1; i < length - 1; i++) {
-    totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
-  }
+    // start at index 1 since at the 0th index the rain cannot be trapped anyways
+    for (let i = 1; i < length - 1; i++) {
+        // our total water is the minimum of the index of either going from left to right or right to left minus the height of the building
+        totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+    }
 
-  return totalWater;
+    return totalWater;
 };
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
