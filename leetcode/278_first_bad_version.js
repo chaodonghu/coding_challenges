dc479acefc50542a0eeb9a0839cdec64885c8eb5
chaodonghu <dojostan@gmail.com>
2021-03-06 13:33:40 -0500

    add binary search explanation to first bad version

--- leetcode/278_first_bad_version.js
+++ leetcode/278_first_bad_version.js
@@ -1,19 +1,3 @@
-// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
-//
-// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
-//
-// You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
-//
-// Example:
-//
-// Given n = 5, and version = 4 is the first bad version.
-//
-// call isBadVersion(3) -> false
-// call isBadVersion(5) -> true
-// call isBadVersion(4) -> true
-//
-// Then 4 is the first bad version.
-
 /**
  * Definition for isBadVersion()
  *
@@ -28,24 +12,33 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        let left = 1;
-        let right = n;
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    // start our two pointers
+    let left = 1;
+    let right = n;
 
-        while (left < right) {
-            let mid = left + Math.floor((right - left) / 2);
-            if (!isBadVersion(mid)) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
+    // while our left is less than our right
+    while (left < right) {
+      // get out mid
+      let mid = left + Math.floor((right - left) / 2);
+      // if mid is not a bad version then that means the first bad version lies on the right of the mid since everything before the mid is not bad
+      if (!isBadVersion(mid)) {
+        left = mid + 1;
+      } else {
+        // if the mid is the bad version then the first bad version must lie to the left of the mid so shrink our binary search
+        right = mid;
+      }
+    }
 
-        return left;
-    };
+    // once left is greater or equal to mid we have our first bad version
+    return left;
+  };
 };
+
+// Time: O(log N), the search is halved in time
+// Space: O(1)
