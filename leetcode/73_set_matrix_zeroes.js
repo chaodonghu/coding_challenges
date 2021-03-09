08572a1719401db6eb4be3103959ea4ccd65456a
chaodonghu <dojostan@gmail.com>
2021-03-08 21:17:34 -0500

    add brute force solution to set matrix zeros

--- leetcode/73_set_matrix_zeroes.js
+++ leetcode/73_set_matrix_zeroes.js
@@ -1,20 +1,58 @@
+// Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+//
+// Follow up:
+//
+// A straight forward solution using O(mn) space is probably a bad idea.
+// A simple improvement uses O(m + n) space, but still not the best solution.
+// Could you devise a constant space solution?
+//
+//
+// Example 1:
+//
+//
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+//
+//
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+//
+//
+// Constraints:
+//
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
+// brute force solution
 var setZeroes = function (matrix) {
-  var r = matrix.length;
-  var l = matrix[0].length;
-  for (var i = 0; i < r; i++) {
-    for (var j = 0; j < l; j++) {
-      if (matrix[i][j] === 0 && 1 / matrix[i][j] === Infinity) {
-        for (var x = 0; x < r; x++) {
-          matrix[x][j] = matrix[x][j] && -0;
-        }
-        for (var y = 0; y < l; y++) {
-          matrix[i][y] = matrix[i][y] && -0;
-        }
+  let rows = [];
+  let cols = [];
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] === 0) {
+        rows.push(row);
+        cols.push(col);
       }
     }
   }
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (rows.includes(row) || cols.includes(col)) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+  return matrix;
 };
+
+// Time: O(M * N)
+// Space: O(M + N)
