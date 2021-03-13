b01fcbeb4d5f9e5fee35da23404ca41725e6cdc4
chaodonghu <dojostan@gmail.com>
2021-03-12 22:17:16 -0500

    add time complexity to longest increasing subsequence, add solution to check if a string contains all binary codes of size k

--- leetcode/300_longest_increasing_subsequence.js
+++ leetcode/300_longest_increasing_subsequence.js
@@ -4,21 +4,24 @@
  */
 function lengthOfLIS(nums) {
   if (!nums.length) return 0;
-  var list = [];
+  var dp = [];
 
   for (var i = 0; i < nums.length; i++) {
-    // push 1 into the list as there must be atleast one subsequence
-    list.push(1);
+    // push 1 into the dp as there must be atleast one subsequence
+    dp.push(1);
     // make another pointer and make it end at the length of the i
     for (var j = 0; j < i; j++) {
       // second pointer needs to be greater or equal to the first pointer
       // if the element is not greater then increase the pointer
       if (nums[j] < nums[i]) {
-        // get the max of the subarrays (list[i] and list[j] + 1)
-        list[i] = Math.max(list[i], list[j] + 1);
+        // get the max of the subarrays (dp[i] and dp[j] + 1)
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
   }
   // return the maximum in the array
-  return Math.max(...list)
+  return Math.max(...dp)
 }
+
+// Time: O(N^2)
+// Space: O(N) since we build a dp to be the size of the nums array with each element being the longest increasing subsequence up until that index
