f6d74ba2c0dd8ee42c6ab2ee5303b95f8d1d34e9
chaodonghu <dojostan@gmail.com>
2021-04-03 11:04:29 -0400

    add explanation to two pass approach on longest valid parentheses and add stack appraoch

--- leetcode/32_longest_valid_parentheses.js
+++ leetcode/32_longest_valid_parentheses.js
@@ -17,46 +17,79 @@
 // Input: s = ""
 // Output: 0
 
+// Two pass approach, constant space
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    let open = 0;
+    let close = 0;
+    let max = 0;
+
+    for (char of S) {
+        if (char === '(') {
+            open++;
+        } else {
+            close++;
+        }
+
+        if (open === close) {
+            max = Math.max(max, open + close);
+            // if there are more closing brackets than open ones then there can't possibly be any more valid parentheses substrings
+        } else if (close > open) {
+            open = 0;
+            close = 0;
+        }
+    }
+
+    // we go backwards through the string
+    for (let i = s.length - 1; i >= 0; i++) {
+        if (s.charAt(i) === '(') {
+            open++;
+        } else {
+            close++;
+        }
+
+        if (open === close) {
+            max = Math.max(max, open + close);
+        // if there are more opening brackets then closing brackets then there can't possibly be any more valid parentheses substrings
+        } else if (open > close) {
+            open = 0;
+            close = 0;
+        }
+    }
+
+};
+
+// Time: O(N) we pass through the string twice, once forwards and the next backwards
+// Space: O(1)
+
+// Stack Approach
 /**
  * @param {string} s
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-  let open = 0;
-  let close = 0;
-  let maxLength = 0;
+  let max = 0;
 
-  for (char of s) {
-    if (char === "(") {
-      open++;
+  let stack = [];
+  stack.push(-1);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === "(") {
+      stack.push(i);
     } else {
-      close++;
-    }
-
-    if (open === close) {
-      maxLength = Math.max(maxLength, open + close);
-    } else if (close > open) {
-      open = 0;
-      close = 0;
+      stack.pop();
+      if (!stack.length) {
+        stack.push(i);
+      } else {
+        max = Math.max(max, i - stack[stack.length - 1]);
+      }
     }
   }
 
-  open = 0;
-  close = 0;
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === "(") {
-      open++;
-    } else {
-      close++;
-    }
-
-    if (open === close) {
-      maxLength = Math.max(maxLength, open + close);
-    } else if (open > close) {
-      open = 0;
-      close = 0;
-    }
-  }
-
-  return maxLength;
+  return max;
 };
+// Time: O(N) since we iterate through all characters of the string
+// Space: O(N) at worst case we would store all of our opening brackets into the stack with the longest length of the valid parentheses being 0
