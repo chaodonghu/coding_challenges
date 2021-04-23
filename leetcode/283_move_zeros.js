8b9a70a815596bb6ff41e21d42e9ada8fb559cae
chaodonghu <dojostan@gmail.com>
2021-04-22 22:54:02 -0400

    update move zeros to the beginning of the array to move zeros answer

--- leetcode/283_move_zeros.js
+++ leetcode/283_move_zeros.js
@@ -12,23 +12,34 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    // anchor refers to the index in nums
-    let anchor = 0;
+var moveZeroes = function (nums) {
+  // anchor refers to the index in nums
+  let anchor = 0;
 
-    for (let explorer = 0; explorer < nums.length; explorer++) {
-        if (nums[explorer] !== 0) {
-            // temp will always be 0;
-            let temp = nums[anchor];
-            nums[anchor] = nums[explorer];
-            nums[explorer] = temp;
+  for (let explorer = 0; explorer < nums.length; explorer++) {
+    if (nums[explorer] !== 0) {
+      // temp will always be 0;
+      let temp = nums[anchor];
+      nums[anchor] = nums[explorer];
+      nums[explorer] = temp;
 
-            anchor++;
-        }
+      anchor++;
     }
-
+  }
 };
-
 
 // Time: O(N) pass through the array once
 // Space: O(1), you have two pointers
+
+let moveZerosToLeft = function (A) {
+  let anchor = A.length - 1;
+
+  for (let explorer = A.length - 1; explorer >= 0; explorer--) {
+    if (A[explorer] !== 0) {
+      let temp = A[anchor];
+      A[anchor] = A[explorer];
+      A[explorer] = temp;
+      anchor--;
+    }
+  }
+};
