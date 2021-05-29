3132ea2ccbbc1073fcb2a272130d54733e58dc5a
chaodonghu <dojostan@gmail.com>
2021-05-29 18:01:42 -0400

    add backtracking solution to n queens and n queens ii

--- leetcode/52_n_queens_ii.js
+++ leetcode/52_n_queens_ii.js
@@ -1,21 +1,30 @@
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+//
+// Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+//
+//
+//
+// Example 1:
+//
+//
+// Input: n = 4
+// Output: 2
+// Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+// Example 2:
+//
+// Input: n = 1
+// Output: 1
+//
+//
+// Constraints:
+//
+// 1 <= n <= 9
+
 /**
  * @param {number} n
  * @return {number}
  */
-
-var formatBoard = (slate) => {
-  let result = [];
-
-  for (let col = 0; col < slate.length; col++) {
-    let row = new Array(slate.length).fill(".");
-    row[slate[col]] = "Q";
-    result.push(row.join(""));
-  }
-
-  return result;
-};
-
-var solveNQueens = function (n) {
+var totalNQueens = function (n) {
   // global result
   const result = [];
 
@@ -56,5 +65,5 @@ var solveNQueens = function (n) {
 
   dfs(0, n, []);
 
-  return result.map((slate) => formatBoard(slate));
+  return result.length;
 };
