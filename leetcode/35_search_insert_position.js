6b646b2c8acb4ea14e35f4a1c63dc8cfc40fd409
chaodonghu <dojostan@gmail.com>
2022-02-25 22:51:03 -0500

    utilize binary search algo for search insert position

--- leetcode/35_search_insert_position.js
+++ leetcode/35_search_insert_position.js
@@ -24,11 +24,30 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target || nums[i] > target) {
-            return i;
-        }
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  // instantiate our pointers
+  let left = 0;
+  let right = nums.length - 1;
+
+  // if our left pointer is less than or equal to our right pointer
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
-    return nums.length;
+  }
+
+  // just return the index where the target would be
+  return left;
 };
+
+// Time: O(log N)
+// Space: O(1)
