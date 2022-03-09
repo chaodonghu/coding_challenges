7749308e8a42c4f27313eb28b22c4fff7de3aa32
chaodonghu <dojostan@gmail.com>
2022-03-08 22:14:48 -0500

    add detailed solution to remove duplicates from sorted list ii

--- leetcode/82_remove_duplicates_from_sorted_list_ii.js
+++ leetcode/82_remove_duplicates_from_sorted_list_ii.js
@@ -1,3 +1,25 @@
+// Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+//
+//
+//
+// Example 1:
+//
+//
+// Input: head = [1,2,3,3,4,4,5]
+// Output: [1,2,5]
+// Example 2:
+//
+//
+// Input: head = [1,1,1,2,3]
+// Output: [2,3]
+//
+//
+// Constraints:
+//
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,26 +32,33 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-  let dummyNode = new ListNode(-1);
-  dummyNode.next = head;
+  let dummyNode = new ListNode(-1, head);
+  // set our pointer
   let node = dummyNode;
 
+  // while we haven't reached the end of the linked list
   while (node.next) {
-    // if the next value equals the next next value
-    if (node.next.next && node.next.val === node.next.next.val) {
+    let nextNode = node.next.next;
+    // if the next two nodes are duplicates
+    if (nextNode && node.next.val === nextNode.val) {
+      // grab the next-next node to point to
       let nonValNode = node.next.next.next;
 
-      // keep going through linked list until we find the value where the current node does not equal the next next next value
+      // until we don't have a duplicate
       while (nonValNode && nonValNode.val === node.next.val) {
         nonValNode = nonValNode.next;
       }
-      // finally set the next node to the non-equal value
+
+      // then move our pointer to this nonValNode which is not a duplicate
       node.next = nonValNode;
     } else {
-      // keep going and the nodes are distinct
+      // if we don't have the duplicate just move our pointer
       node = node.next;
     }
   }
 
   return dummyNode.next;
 };
+
+// Time: O(N)
+// Space; O(1)
