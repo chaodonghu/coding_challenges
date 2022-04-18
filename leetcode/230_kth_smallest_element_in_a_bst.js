6501adaa9254d8b04040ca971ca887aae76f0808
chaodonghu <dojostan@gmail.com>
2022-04-17 22:13:22 -0400

    add iterative solution to find kth smallest element in a bst

--- leetcode/230_kth_smallest_element_in_a_bst.js
+++ leetcode/230_kth_smallest_element_in_a_bst.js
@@ -45,26 +45,51 @@
  * @param {number} k
  * @return {number}
  */
-var kthSmallest = function(root, k) {
-    let array = [];
 
-    var dfs = function(node) {
-        if (!node || array.length > k) {
-            return;
-        }
+// the idea is to build an inorder traversal of BST which is an array sorted in ascending order
+var kthSmallest = function (root, k) {
+  let array = [];
 
-        // recursively call on the left most
-        dfs(node.left);
-        // push into the array starting from the left most element (smallest)
-        array.push(node.val);
-        dfs(node.right);
+  var dfs = function (node) {
+    if (!node || array.length > k) {
+      return;
     }
 
-    // call depth first search on the root first
-    dfs(root)
-    // return the kth element of the array (index starts at 0)
-    return array[k-1]
+    // recursively call on the left most
+    dfs(node.left);
+    // push into the array starting from the left most element (smallest)
+    array.push(node.val);
+    dfs(node.right);
+  };
+
+  // call depth first search on the root first
+  dfs(root);
+  // return the kth element of the array (index starts at 0)
+  return array[k - 1];
 };
 
 // Time: O(H) wtih H being the height of the tree
 // Space: O(N) with N being the number of nodes in the tree in the worst case
+
+// Iterative solution
+var kthSmallest = function (root, k) {
+  const stack = [];
+
+  while (true) {
+    // push our root into the stack until we reach the leaf nodes of our left subtree
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    // take our last left tree which will be the lowest element in our subtree
+    root = stack.pop();
+    // decrement the k counter
+    k -= 1;
+    // if our k is at 0 then return the val
+    if (!k) {
+      return root.val;
+    }
+    // if not go to the right subtree
+    root = root.right;
+  }
+};
