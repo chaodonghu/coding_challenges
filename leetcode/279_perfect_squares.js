4335f14f755ce5867cf54f9052546d06f5dfd207
chaodonghu <dojostan@gmail.com>
2022-11-22 10:05:51 -0500

    update perfect squares solution

--- leetcode/279_perfect_squares.js
+++ leetcode/279_perfect_squares.js
@@ -15,13 +15,23 @@
  * @param {number} n
  * @return {number}
  */
-var numSquares = function(n) {
-    let dp = new Array(n+1).fill(Number.MAX_VALUE);
-    dp[0] = 0;
-    for (let i=1;i<=n;i++) {
-        for (let j=1;j<=Math.floor(Math.sqrt(i));j++){
-            dp[i] = Math.min(dp[i], dp[i-j*j]+1);
-        }
+var numSquares = function (n) {
+  let dp = new Array(n + 1).fill(Number.MAX_VALUE);
+  dp[0] = 0;
+
+  // Go through 1 to n
+  for (let i = 1; i <= n; i++) {
+    // Go through each iteration of a square until we hit our target n
+    for (let j = 1; j <= i + 1; j++) {
+      // Square the loop
+      let square = j * j;
+      // If our target - square is less than 0 then break out of our loop
+      if (i - square < 0) {
+        break;
+      }
+      // The minimum of squares of the current target or 1 + the target - square
+      dp[i] = Math.min(dp[i], 1 + dp[i - square]);
     }
-    return dp[n];
-}
+  }
+  return dp[n];
+};
