a64373d22510f0e065ba8cb7704e01746ac1177c
chaodonghu <dojostan@gmail.com>
2023-10-24 16:26:32 -0400

    add bfs solution to find largest value in each tree row

--- leetcode/515_find_largest_value_in_each_tree_row.js
+++ leetcode/515_find_largest_value_in_each_tree_row.js
@@ -50,3 +50,32 @@ var largestValues = function (root) {
 
 // Time: O(N), we visit each node in the tree once
 // Space: O(N), all nodes will be eventually in the queue
+
+// DFS
+var largestValues = function(root) {
+    const result = []
+
+    var dfs = (node, depth) => {
+        if (!node) return;
+
+        // initialize the current node.val as the maximum value seen at depth to keep track of the level
+        if (depth === result.length) {
+            result.push(node.val);
+        } else {
+            // every subsequent node in the level will be compared to the current max
+            result[depth] = Math.max(result[depth], node.val);
+        }
+
+        // execute the dfs on the left and the right nodes if there are any
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1)
+    }
+
+    // start at the root node
+    dfs(root, 0);
+    return result;
+ };
+
+ // Time: O(N)
+ // Space: O(N)
+
