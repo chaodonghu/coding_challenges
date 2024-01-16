899be652ca25e0e2231ede855a1efc3d33a59e8d
chaodonghu <dojostan@gmail.com>
2024-01-16 11:13:09 -0500

    update insert delete get random solution to use set instead

--- leetcode/380_insert_delete_get_random.js
+++ leetcode/380_insert_delete_get_random.js
@@ -20,7 +20,46 @@
 // // getRandom should return either 1 or 2 randomly.
 // randomSet.getRandom();
 //
-// // Removes 1 from the set, returns true. Set now contains [2].
+// // Removes 1 from the set, returns true. Set now contains [2].a
+var RandomizedSet = function() {
+    this.set = new Set();
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.set.has(val)) return false;
+
+    this.set.add(val);
+    return true;
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    return this.set.delete(val)   
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    let randomIndex = Math.floor(Math.random() * this.set.size);
+    let arr = [...this.set];
+    return arr[randomIndex];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
 // randomSet.remove(1);
 //
 // // 2 was already in the set, so return false.
