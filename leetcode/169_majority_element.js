ea37a078299f8e1dadb4599bc56767986a142fb9
chaodonghu <dojostan@gmail.com>
2024-02-12 01:05:21 -0500

    refine majority element solution

--- leetcode/169_majority_element.js
+++ leetcode/169_majority_element.js
@@ -1,19 +1,3 @@
-// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
-//
-// You may assume that the array is non-empty and the majority element always exist in the array.
-//
-// Example 1:
-//
-// Input: [3,2,3]
-// Output: 3
-// Example 2:
-//
-// Input: [2,2,1,1,1,2,2]
-// Output: 2
-/**
- * @param {number[]} nums
- * @return {number}
- */
 /**
  * @param {number[]} nums
  * @return {number}
@@ -24,12 +8,12 @@ var majorityElement = function (nums) {
   let maxNum = 0;
 
   for (let num of nums) {
-    countMap.has(num)
-      ? countMap.set(num, countMap.get(num) + 1)
-      : countMap.set(num, 1);
+    countMap.set(num, (countMap.get(num) || 0) + 1);
 
-    if (countMap.get(num) > max) {
-      max = countMap.get(num);
+    const currentNumCount = countMap.get(num);
+    // if the current num count is greater than the max
+    if (currentNumCount > max) {
+      max = currentNumCount;
       maxNum = num;
     }
   }
