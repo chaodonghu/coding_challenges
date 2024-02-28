b67acad9b965ea524c553b4adb2fd5f9ba27229b
chaodonghu <dojostan@gmail.com>
2024-02-28 12:50:16 -0500

    add bfs solution to find bottom left tree value

--- leetcode/513_find_bottom_left_tree_value.js
+++ leetcode/513_find_bottom_left_tree_value.js
@@ -10,6 +10,9 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+
+// dfs solution
 var findBottomLeftValue = function (root) {
   // initialize max depth and bottom left value
   let maxDepth = -1;
@@ -37,3 +40,39 @@ var findBottomLeftValue = function (root) {
 
 // Time: O(N)
 // Space: O(N)
+
+// bfs solution
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    let arr = []
+    let queue = [root];
+
+    while (queue.length) {
+        // get the first value in the queue
+        let curr = queue.shift();
+        arr.push(curr.val);
+
+        // go level by level and push into the queue from right to left
+        if (curr.right) {
+            queue.push(curr.right);
+        }
+
+        if (curr.left) {
+            queue.push(curr.left);
+        }
+    }
+
+    // get the last value in the array
+    return arr[arr.length - 1];
+};
