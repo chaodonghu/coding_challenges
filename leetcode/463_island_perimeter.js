4f81fabfabe075cdccc79914d6fe25e592aa1621
chaodonghu <dojostan@gmail.com>
2024-04-18 15:58:55 -0400

    update island perimeter solution

--- leetcode/463_island_perimeter.js
+++ leetcode/463_island_perimeter.js
@@ -7,15 +7,15 @@ var islandPerimeter = function (grid) {
   // loop through the rows
   grid.forEach((row, r) => {
     // loop through the columns
-    row.forEach((col, c) => {
-      // get the value (1) of the left, right, top and bottom of the coordinate or it is 0
+    row.forEach((currentCell, c) => {
+      // Determine if the cells to the left, right, top and bottom of the current cell is land or water, if it is land, it is 1 if it it water it is 0
       var left = grid[r][c - 1] || 0;
       var right = grid[r][c + 1] || 0;
       var top = (grid[r - 1] || [])[c] || 0;
       var bottom = (grid[r - 1] || [])[c] || 0;
 
-      // if the column is 1 then add the edges of that coordinate (subtract 4 from the total of the top, bottom, left, right)
-      if (col === 1) {
+      // if the current cell is 1 then add the edges of that coordinate (subtract 4 from the total of the top, bottom, left, right)
+      if (currentCell === 1) {
         edges += 4 - (top + bottom + left + right);
       }
     });
