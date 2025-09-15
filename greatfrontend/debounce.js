1cc43e2ef2a001ddf50f05c8cbdb8d88f2845051
Dong Hu <dong.hu@zapier.com>
2025-09-14 23:30:58 -0400

    add debounce solution

---
+++ greatfrontend/debounce.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} func
+ * @param {number} wait
+ * @return {Function}
+ */
+export default function debounce(func, wait) {
+  // We need a timeoutId incase the debounced function is called again
+  let timeoutId;
+  // utilize function rather than arrow function to preserve the 'this' context
+  return function (...args) {
+    // if the debounced function is called again, clear the timeout and set a new timeout
+    if (timeoutId) clearTimeout(timeoutId);
+    // set a new timeout Id
+    timeoutId = setTimeout(() => {
+      // has the same 'this' as the outer function's as it's within an arrow function
+      func.apply(this, args);
+    }, wait);
+  };
+}
+
+// Example usage
+function sayHello() {
+  console.log("My name is", this.name);
+}
+
+const amy = {
+  name: "amy",
+  speak: debounce(sayHello),
+};
+
+amy.speak();
